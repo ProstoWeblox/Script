d89971390a4d94b40e668b27e1f6de268e07bedd
ProstoWeblox <150521847+ProstoWeblox@users.noreply.github.com>
2023-11-11 16:56:23 +0500

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    void Update()
+    {
+        // Выводит каждый кадр сообщение в конслоь
+        print("Crickets");
+    }
+}
